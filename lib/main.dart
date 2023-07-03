be1374d47cd16328b490b2518171dd40d427db46
Txt-exe <pekinfo1@gmail.com>
2023-07-03 17:56:45 -0500

    Added Function to request data from flask server

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,24 @@
 // Step 1
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
+import 'package:movieapp/utils/utils/requestdata.dart';
+
+Future printData() async {
+
+  var data = await RequestData.getSomeData(Uri.parse('http://10.0.2.2:5000//mpopular_m'));
+  var decodedData = jsonDecode(data);
+  print(decodedData['results']);
+  print("hi");
+
+}
 void main() {
 // Giving command to runApp() to run the app.
 
 /* The purpose of the runApp() function is to attach
 the given widget to the screen. */
   runApp(StreamexApp());
+  printData();
 }
 
 class StreamexApp extends StatefulWidget{
@@ -19,17 +31,15 @@ class StreamexApp extends StatefulWidget{
 
 class _StreamexAppState extends State<StreamexApp>{
 
-  Future<void> getSomedata(url) async {
-    Response response = await get(url);
-    
 
-  }
+
+
   Widget build(BuildContext context) {
+
     return MaterialApp(
       home: Scaffold(
 
         body: Container(
-          
 
 
         ),
---
+++ lib/utils/utils/requestdata.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart';
+
+class RequestData extends StatelessWidget{
+  const RequestData({super.key});
+
+  testFunc(){
+
+
+  }
+  static Future getSomeData(url) async {
+    Response response = await get(url);
+    return response.body;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+   return Container(
+
+
+
+   );
+  }
+
+
+
+}
