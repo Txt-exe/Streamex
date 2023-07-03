c09754c6ba8da180c89fd11efed5fc69a09503b2
Txt-exe <pekinfo1@gmail.com>
2023-07-03 14:45:10 -0500

    Removed unused variable

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,36 @@
 // Step 1
+import 'package:flutter/material.dart';
+
+void main() {
+// Giving command to runApp() to run the app.
+
+/* The purpose of the runApp() function is to attach
+the given widget to the screen. */
+  runApp(StreamexApp());
+}
+
+class StreamexApp extends StatefulWidget{
+  const StreamexApp({ super.key });
+
+  @override
+  State<StreamexApp> createState() => _StreamexAppState();
+
+  }
+
+class _StreamexAppState extends State<StreamexApp>{
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+
+        body: Container(
+          
+
+
+        ),
+      ),
+    );
+  }
+
+
+}
