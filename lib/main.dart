c79162c832cfa328fda371001b852b16a9b8a961
Txt-exe <pekinfo1@gmail.com>
2023-07-04 11:07:05 -0500

    removed unused code and fixed function errors

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,16 @@
 // Step 1
-import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:http/http.dart';
-import 'package:movieapp/utils/utils/requestdata.dart';
 import 'package:logger/logger.dart';
 
 Logger newlog = Logger();
 
-//Test Function for testing connection with python server
-Future printData(bool debug_option) async {
-
-  var data = await RequestData.getMediaData(Uri.parse('http://10.0.2.2:5000/mpopular_m'));
-  var decodedData = jsonDecode(data);
-
-  if(debug_option == true){
-
-    newlog.i(decodedData['results']);
-  }
-    else{
-//
-
-  }
-}
 void main() {
 // Giving command to runApp() to run the app.
 
 /* The purpose of the runApp() function is to attach
 the given widget to the screen. */
   runApp(const StreamexApp());
-  printData(false);
 }
 
 class StreamexApp extends StatefulWidget{
